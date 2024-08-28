96fdb497297a77985ab24b0768572a865ccf1d2e
Favour Emmanuel <iyenefavour@gmail.com>
2024-08-28 23:55:06 +0100

    updated cart components

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 
-const Cart = ({ cartItems, onClear }) => {
-  return (
-    <>
-      <div>Cart:</div>
-      <ul>
-        {cartItems.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-      <button onClick={onClear} className="bg-pink-400 py-2 px-8 ">
-        Clear
-      </button>
-    </>
-  );
+const cart = () => {
+  return <div>cart</div>;
 };
 
-export default Cart;
+export default cart;
