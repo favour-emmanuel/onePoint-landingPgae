914c14f75e5fbfb97f2bc7e74ccad543ae123193
Favour Emmanuel <iyenefavour@gmail.com>
2024-09-30 12:45:16 +0100

    removed unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Navball from "./components/Navball";
+import React from "react";
 import Example from "./components/Example";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Navball cartItemsCount={cartItems.length} />
       <Example />
     </>
   );
--- src/components/Navball.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Navball = ({ cartItemsCount }) => {
-  return <div>Navball: {cartItemsCount} </div>;
-};
-
-export default Navball;
